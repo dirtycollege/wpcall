ec0fa18afc209e9980917a0a0854bb1ffc33cc86
dirtycollege <Ncertcollege@gmail.com>
2026-02-16 09:33:25 +0530

    uju

--- users.js
+++ users.js
@@ -1,10 +1,10 @@
-const USERS = {
-  neha:   { name:"Neha",   dp:"assets/neha.jpg",    video:"assets/neha.mp4" },
-  radha:  { name:"Radha",  dp:"assets/radha.jpg",   video:"assets/radha.mp4" },
-  kajal:  { name:"Kajal",  dp:"assets/kajal.jpg",   video:"assets/kajal.mp4" },
-  kamini: { name:"Kamini", dp:"assets/kamini.jpg",  video:"assets/kamini.mp4" },
-  pari:   { name:"Pari",   dp:"assets/pari.jpg",    video:"assets/pari.mp4" },
-  sweta:  { name:"Sweta",  dp:"assets/sweta.jpg",   video:"assets/sweta.mp4" },
-  ragini: { name:"Ragini", dp:"assets/ragini.jpg",  video:"assets/ragini.mp4" },
-  sneha:  { name:"Sneha",  dp:"assets/sneha.jpg",   video:"assets/sneha.mp4" }
+const USERS={
+neha:{name:"neha",dp:"assets/neha.jpg",video:"assets/neha.mp4"},
+radha:{name:"Radha",dp:"assets/Radha.jpg",video:"assets/radha.mp4"},
+kajal:{name:"Kajal",dp:"assets/Kajal.jpg",video:"assets/kajal.mp4"},
+kamini:{name:"Kamini",dp:"assets/Kamini.jpg",video:"assets/kamini.mp4"},
+pari:{name:"Pari",dp:"assets/Pari.jpg",video:"assets/pari.mp4"},
+sweta:{name:"Sweta",dp:"assets/Sweta.jpg",video:"assets/sweta.mp4"},
+ragini:{name:"Ragini",dp:"assets/Ragini.jpg",video:"assets/ragini.mp4"},
+sneha:{name:"Sneha",dp:"assets/Sneha.jpg",video:"assets/sneha.mp4"}
 };
