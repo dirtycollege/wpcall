b25d1134de5b9b599cd0edb931e44be4e802618d
dirtycollege <Ncertcollege@gmail.com>
2026-02-16 20:04:08 +0530

    uyt

--- users.js
+++ users.js
@@ -6,5 +6,5 @@ kamini:{name:"kamini",dp:"assets/kamini.jpg",video:"assets/kamini.mp4"},
 pari:{name:"pari",dp:"assets/pari.jpg",video:"assets/pari.mp4"},
 sweta:{name:"sweta",dp:"assets/sweta.jpg",video:"assets/sweta.mp4"},
 ragini:{name:"ragini",dp:"assets/ragini.jpg",video:"assets/neha.mp4"},
-sneha:{name:"sneha",dp:"assets/sneha.jpg",video:"assets/kamini.mp4"}
+sneha:{name:"sneha",dp:"assets/sneha.jpg",video:"assets/sweta.mp4"}
 };
