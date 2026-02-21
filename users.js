44e0aae721447f6d202b3d6ddaae665633d5149e
dirtycollege <Ncertcollege@gmail.com>
2026-02-21 19:15:41 +0530

    uip

--- users.js
+++ users.js
@@ -1,10 +1,10 @@
 const USERS={
-neha:{name:"neha",dp:"assets/neha.jpg",video:"assets/neha.mp4"},
+neha:{name:"neha",dp:"assets/neha.jpg",video:"assets/pari.mp4"},
 radha:{name:"radha",dp:"assets/radha.jpg",video:"assets/sweta.mp4"},
-kajal:{name:"kajal",dp:"assets/kajal.jpg",video:"assets/neha.mp4"},
+kajal:{name:"kajal",dp:"assets/kajal.jpg",video:"assets/kamini.mp4"},
 kamini:{name:"kamini",dp:"assets/kamini.jpg",video:"assets/kamini.mp4"},
 pari:{name:"pari",dp:"assets/pari.jpg",video:"assets/pari.mp4"},
 sweta:{name:"sweta",dp:"assets/sweta.jpg",video:"assets/sweta.mp4"},
 ragini:{name:"ragini",dp:"assets/ragini.jpg",video:"assets/neha.mp4"},
-sneha:{name:"sneha",dp:"assets/sneha.jpg",video:"assets/sweta.mp4"}
+sneha:{name:"sneha",dp:"assets/sneha.jpg",video:"assets/pari.mp4"}
 };
