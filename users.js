88a09f3c1a9fc796f448c15ad9546715920b6abd
dirtycollege <Ncertcollege@gmail.com>
2026-05-03 09:41:01 +0530

    abh

--- users.js
+++ users.js
@@ -5,6 +5,6 @@ kajal:{name:"kajal",dp:"assets/kajal.jpg",video:"assets/kamini.mp4"},
 kamini:{name:"kamini",dp:"assets/kamini.jpg",video:"assets/kamini.mp4"},
 pari:{name:"pari",dp:"assets/pari.jpg",video:"assets/pari.mp4"},
 sweta:{name:"sweta",dp:"assets/sweta.jpg",video:"assets/kamini.mp4"},
-ragini:{name:"ragini",dp:"assets/ragini.jpg",video:"assets/neha.mp4"},
+ragini:{name:"ragini",dp:"assets/ragini.jpg",video:"assets/pari.mp4"},
 sneha:{name:"sneha",dp:"assets/sneha.jpg",video:"assets/pari.mp4"}
 };
